f60cf40ed33b13f300ff97624af646dd10efabf7
janki] <jankisavsaviya2018@gmail.com>
2023-09-20 12:26:55 +0530

    upadate

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./index.css";
-
+import {PiInstagramLogoThin} from 'react-icons/pi'
+import {RiFacebookLine} from 'react-icons/ri'
 export default function Team() {
   return (
     <>
-      <div className="team_bg">
+      <div>
         <div>
           <h2 className="small_team_heading">meet our team</h2>
           <h1 className="team_heading">Team Members</h1>
@@ -13,13 +14,9 @@ export default function Team() {
           <div className="team">
             <img className="img_team" src="/image/team-1.png" alt="" />
             <div className="card">
-              <div className="logo_flex">
-                <h1 className="card_heading">Jone Due</h1>
-                <img src="/image/014-instagram.png" alt="" style={{ margin: ' 15px 0 0 80px', height: '5%' }} />
-              </div>
-
-              <div >
-                <p className="card_small_heading">Printing Lead  <img src="/image/013-facebook.png" alt="" className="facebook" /></p>
+            <div >
+                <h1 className="card_heading">Jone Due  <PiInstagramLogoThin className="insta"/> </h1> 
+                <p className="card_small_heading">Printing Lead  <RiFacebookLine className="facebook"/></p>
               </div>
 
             </div>
@@ -28,13 +25,9 @@ export default function Team() {
           <div  className="team">
             <img className="img_team" src="/image/team-2.png" alt="" />
             <div className="card">
-            <div className="logo_flex">
-                <h1 className="card_heading">Jone Due</h1>
-                <img src="/image/014-instagram.png" alt="" style={{ margin: ' 15px 0 0 80px', height: '5%' }} />
-              </div>
-
-              <div >
-                <p className="card_small_heading">Printing Lead  <img src="/image/013-facebook.png" alt="" className="facebook" /></p>
+            <div >
+                <h1 className="card_heading">Jone Due  <PiInstagramLogoThin className="insta"/> </h1> 
+                <p className="card_small_heading">Printing Lead  <RiFacebookLine className="facebook"/></p>
               </div>
             </div>
           </div>
@@ -42,13 +35,9 @@ export default function Team() {
           <div className="team">
             <img className="img_team" src="/image/team-3.png" alt="" />
             <div className="card">
-            <div className="logo_flex">
-                <h1 className="card_heading">Jone Due</h1>
-                <img src="/image/014-instagram.png" alt="" style={{ margin: ' 15px 0 0 80px', height: '5%' }} />
-              </div>
-
-              <div >
-                <p className="card_small_heading">Printing Lead  <img src="/image/013-facebook.png" alt="" className="facebook" /></p>
+            <div >
+                <h1 className="card_heading">Jone Due  <PiInstagramLogoThin className="insta"/> </h1> 
+                <p className="card_small_heading">Printing Lead  <RiFacebookLine className="facebook"/></p>
               </div>
             </div>
           </div>
@@ -56,13 +45,9 @@ export default function Team() {
           <div className="team">
             <img className="img_team" src="/image/team-05.png" alt="" />
             <div className="card">
-            <div className="logo_flex">
-                <h1 className="card_heading">Jone Due</h1>
-                <img src="/image/014-instagram.png" alt="" style={{ margin: ' 15px 0 0 80px', height: '5%' }} />
-              </div>
-
-              <div >
-                <p className="card_small_heading">Printing Lead  <img src="/image/013-facebook.png" alt="" className="facebook" /></p>
+            <div >
+                <h1 className="card_heading">Jone Due  <PiInstagramLogoThin className="insta"/> </h1> 
+                <p className="card_small_heading">Printing Lead  <RiFacebookLine className="facebook"/></p>
               </div>
             </div>
           </div>
